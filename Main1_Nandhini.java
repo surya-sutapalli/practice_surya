71c462d4a9cfe081a807f87581ed614d6af3e0d2
Nandhini K <71819525+Nandhinkandasamy@users.noreply.github.com>
2021-09-28 12:33:04 +0530

    Program 1 Done by Nandhini.K

---
+++ Main1_Nandhini.java
@@ -0,0 +1,30 @@
+package Test;
+import java.util.*;
+class UserMainCode
+{
+	public static void getLargestWord(String input)
+	{
+		int maxLength=0;
+		String output=" ";
+		String arr[]=input.split(" ");
+		for(String brr:arr)
+		{
+			if(brr.length()>maxLength)
+			{
+				maxLength=brr.length();
+				output=brr;
+			}
+		}
+		System.out.println(output);
+	}
+
+}
+public class Main1_Nandhini 
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		String input=s.nextLine();
+		UserMainCode.getLargestWord(input);
+	}
+}
