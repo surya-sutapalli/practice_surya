081f895133cc7e6c8efbf993b6a005daa89d6e6e
riyas-07 <51043129+riyas-07@users.noreply.github.com>
2021-09-28 12:24:50 +0530

    Program 2 done by Riyasudeen M

---
+++ Main1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class UserMainCode1{
+	public static String formNewWord(String str,int n) {
+		int l=str.length()-1;
+		int h=l-n;
+		String s1=str.substring(0, n);
+		String s2=str.substring(h+1,l+1);
+		String newWord=s1.concat(s2);
+		return newWord;
+
+
+	}
+}
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int n=sc.nextInt();
+		String newWord=UserMainCode1.formNewWord(str, n);
+		System.out.println(newWord);
+
+
+	}
+
+}
