400539e46056bcab1981b573c3ec6c2ce9e0bc15
shriramstark <52104294+shriramstark@users.noreply.github.com>
2021-09-28 12:53:40 +0530

    Program 9 by Shriram

---
+++ Question9_Shriram.java
@@ -0,0 +1,12 @@
+
+public class Question9_Shriram {
+	
+	public static void main(String[] args) {
+		int arr[] = {10, 15, 20, 25, 30, 100};
+		int n = 15;
+		int resul = UserMainCode3_Shriram.addAndReverse(arr, n);
+		System.out.println(resul);
+
+	}
+
+}
