16f6343699ea6a716057c003041a8ee7ec81b5be
thanish31 <91466162+thanish31@users.noreply.github.com>
2021-09-28 13:12:40 +0530

    Program 1 Done by Thanish A

---
+++ UserMainCode1_Thanish.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class UserMainCode1_Thanish 
+{
+	static String getLargestWord(String s)
+	{
+		String crr[] = s.split(" ");
+		String largest = crr[0];
+		for(int i=1; i<crr.length; i++)
+		{
+			String s1 = crr[i];
+			if (s1.length()>largest.length())
+			{
+				largest = s1;
+			}
+			else if(s1.length()==largest.length())
+			{
+				largest = largest;
+			}
+		}
+		return largest;	
+	}
+
+
+	public static void main(String[] args)
+	{
+		String s;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		s= sc.nextLine();
+		UserMainCode1_Thanish obj = new UserMainCode1_Thanish();
+		System.out.println(obj.getLargestWord(s));
+	}
+	
+
+}
