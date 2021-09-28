cf5d7c70d56abb45567746467f513f112c71c415
riyas-07 <51043129+riyas-07@users.noreply.github.com>
2021-09-28 12:23:08 +0530

    Program 1 done by Riyasudeen M

--- Main.java
+++ Main.java
@@ -1,30 +1,31 @@
-package Test;
-import java.util.*;
-class UserMainCode
-{
-	public static void getLargestWord(String input)
-	{
-		int maxLength=0;
-		String output=" ";
-		String arr[]=input.split(" ");
-		for(String brr:arr)
-		{
-			if(brr.length()>maxLength)
-			{
-				maxLength=brr.length();
-				output=brr;
+import java.util.Scanner;
+
+class UserMainCode{
+	public static String getLargestWord(String str) {
+		String arr[]=str.split("\\s+");
+		int l=0;
+		String longestWord=null;
+		for(String brr:arr) {
+			if(brr.length()>l) {
+				l=brr.length();
+				longestWord =brr;
+
 			}
-		}
-		System.out.println(output);
+		}return longestWord;
+
 	}
 
 }
-public class Main 
-{
-	public static void main(String[] args)
-	{
-		Scanner s=new Scanner(System.in);
-		String input=s.nextLine();
-		UserMainCode.getLargestWord(input);
+public class Main {
+
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String longestWord=UserMainCode.getLargestWord(str);
+		System.out.println(longestWord);
+
+
 	}
+
 }
