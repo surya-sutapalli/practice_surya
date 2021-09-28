806770b02bbc4bbef9e7fec90772a3d5aeeefad7
MayankNagpal <60818955+mayank-nagpal@users.noreply.github.com>
2021-09-28 12:31:38 +0530

    Program No5 by Mayank Nagpal

---
+++ UserMainCode2_Mayank.java
@@ -0,0 +1,26 @@
+	
+public class UserMainCode2_Mayank {
+	public static int getSumOfEvenNumber(int n) {
+		int sum=0;
+		String s = Integer.toString(n);
+		int arr[] = new int[s.length()];
+		for(int i=0;i<s.length();i++) {
+			arr[i]= s.charAt(i)-'0';
+		}
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i]%2==0) {
+				
+				sum= sum+ (arr[i]*arr[i]);
+			}
+		}
+		return sum;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(UserMainCode2_Mayank.getSumOfEvenNumber(56895));
+
+	}
+
+}
