01224d655cc84b55b87452c5f0295a74ad3d5ca6
Chetan93-max <81320090+Chetan93-max@users.noreply.github.com>
2021-09-28 12:37:14 +0530

    done by chetan R

---
+++ UserMainCode1_chetan.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class mainCode{
+	public static String getLarWord(String str)
+	{
+		int flg= 0;
+		int max=0;
+		String s3 = "";
+		String[] st = str.split(" ");
+		for(String s2:st)
+		{
+			if(s2.length()>max )
+			{
+				if(s2.length()==max) {
+			
+			
+				flg=1;
+				continue;
+			}	
+			max=s2.length();
+			s3=s2;
+		}
+		}
+		
+		if(flg==1)
+		{
+			System.out.println();
+		}
+		return s3;
+				
+	}
+}
+
+public class UserMainCode1_chetan
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the string");
+		
+		String str = sc.nextLine();
+		
+		System.out.println(mainCode.getLarWord(str));
+	}
+
+}
