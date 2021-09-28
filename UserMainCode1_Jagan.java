d61cd6a2c2d0c72c5a937f9e48c0cbf9cbae575a
Jagan26042000 <91466093+Jagan26042000@users.noreply.github.com>
2021-09-28 12:42:14 +0530

    Add files via upload
    Program done by Jagan S

---
+++ UserMainCode1_Jagan.java
@@ -0,0 +1,33 @@
+//Question number-3.
+import java.util.*;
+public class UserMainCode1_Jagan 
+{
+	public static int getLowest(HashMap<Integer,Integer> h1) 
+	{
+		ArrayList<Integer> arraylist = new ArrayList<Integer>();
+		int temp = 0;
+		Iterator<Integer> itr = h1.keySet().iterator();
+		while (itr.hasNext()) 
+		{
+			int value = itr.next();
+			arraylist.add(h1.get(value));
+		}
+		Collections.sort(arraylist);
+		temp = arraylist.get(0) + arraylist.get(1) + arraylist.get(2);
+		return temp;
+	}
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		//System.out.println("Enter the size :");
+		int n = scan.nextInt();
+		HashMap<Integer,Integer> hashmap = new HashMap<Integer,Integer>();
+		for (int i = 1; i <= n; i++) 
+		{
+			hashmap.put(scan.nextInt(), scan.nextInt());
+		}
+		System.out.println("Output "+getLowest(hashmap));
+		scan.close();
+	}
+
+}
