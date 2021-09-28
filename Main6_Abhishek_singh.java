a55794765550b1e71f688359a1b473af12168309
Abhishek Singh <65482891+abhishek250499@users.noreply.github.com>
2021-09-28 12:49:45 +0530

    Program 6 by Abhishek Singh

---
+++ Main6_Abhishek_singh.java
@@ -0,0 +1,15 @@
+package AbhishekSingh;
+
+import java.util.Scanner;
+
+public class Main6_Abhishek_singh {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String st=sc.nextLine();
+		int result=UserMainCode6_Abhishek_Singh.check(st);
+		System.out.println(result);
+	}
+
+}
