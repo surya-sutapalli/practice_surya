3d4f1e7fb5a8fcb37a425dbe3981cc1c0dcf5084
Kavin <kavin.chendhur@gmail.com>
2021-09-28 12:18:57 +0530

    Program 1 done by Kavin Chendhur  K M

---
+++ UserMainCode1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class UserMainCode1 {
+	
+   static String getLargestWord(String s)
+   {
+	   int lar=0;
+	   String[] comp=s.split(" ");
+	   for(int i=0;i<comp.length;i++)
+	   {
+		   for(int j=i+1;i<comp.length;i++)
+		   if(comp[i].length()>comp[j].length())
+		 {
+			 lar=i;
+		 }
+	   }
+	  
+	   
+	   
+	   
+	   return comp[lar];
+   }
+	public static void main(String[] args) {
+		String s;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Sentence");
+		s=sc.nextLine();
+		
+		String lar=UserMainCode1.getLargestWord(s);
+		System.out.println(lar);
+	}
+
+}
