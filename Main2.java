11e9aaff1a7f5c8cf8bb827f76a64b3203fcbb41
EC7sbzgZ <91466149+EC7sbzgZ@users.noreply.github.com>
2021-09-28 12:42:45 +0530

    Add files via upload
    main1_kaviya

--- Main2.java
+++ Main2.java
@@ -1,15 +1,31 @@
-package AbhishekSingh;
-//2
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main2 {
 
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		String st=sc.next();
-		int n=sc.nextInt();
-		UserMainCode2.formNewWord(st, n);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	    int n = Integer.parseInt(sc.nextLine());
+	    HashMap<Integer, Integer> h1 = new HashMap<Integer, Integer>();
+	    for (int i = 0; i < n; i++) {
+	      h1.put(sc.nextInt(), sc.nextInt());
+	    }
+	    System.out.println(getvalues(h1));
+	  }
+	  public static int getvalues(HashMap<Integer, Integer> h1) {
+	    ArrayList<Integer> a1 = new ArrayList<Integer>();
+	    int m = 0;
+	    Iterator<Integer> it = h1.keySet().iterator();
+	    while (it.hasNext()) {
+	      int x = it.next();
+	      a1.add(h1.get(x));
+	    }
+	    Collections.sort(a1);
+	    m = a1.get(0) + a1.get(1) + a1.get(2);
+	    return m;
 
 	}
 
