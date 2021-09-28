4577bb3df483b631d513b4b7015b38953a1d0729
Gaurav kumar <91466039+gaurav7kumar@users.noreply.github.com>
2021-09-28 12:27:23 +0530

    Add files via upload
    Submitted By Gaurav que1

--- UserMainCode1.java
+++ UserMainCode1.java
@@ -1,47 +1,30 @@
-import java.util.Scanner;
+package Gaurav;
+import java.util.*;
 
-public class UserMainCode1 {
-	public static int addandreverse(int brr[],int n2)
+public class UserMainCode1{
+	static String getLargestWord(String s)
+	   {
+		   int t=0;
+		   String[] comp=s.split(" ");
+		   for(int i=0;i<comp.length;i++)
+		   {
+			   for(int j=i+1;i<comp.length;i++)
+			   if(comp[i].length()>comp[j].length())
+			 {
+				 t=i;
+			 }
+		   }
+		   return comp[t];
+	   }
+	public static void main(String[] args) 
 	{
-		int sum=0;
-		int rev=0;
-		for(int num:brr)
-		{
-			if(num>n2)
-			{
-				sum+=num;
-			}
-		}
-		System.out.println(sum);
-				
-				
-			while(sum!=0)
-			{
-				int rem=sum%10;
-			    rev=rev*10+rem;
-				sum=sum/10;
-			}
-			return rev;
-			
-	}
+		String s;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Sentence");
+		s=sc.nextLine();
 
-	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		
-		System.out.println("Enter number of elements in an array:");
-		int n=sc.nextInt();
-		int arr[]=new int[n];
-		System.out.println("Enter the elements:");
-		for(int i=0;i<n;i++)
-		{
-			 arr[i]=sc.nextInt();
-		}
-		System.out.println("Enter the element:");
-		int n2=sc.nextInt();
-		UserMainCode1 obj=new UserMainCode1();
-		System.out.println(obj.addandreverse(arr, n2));
-	
-
+		String lar=UserMainCode1.getLargestWord(s);
+		System.out.println(lar);
 	}
 
 }
