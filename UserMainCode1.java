827d75c936984a82ec6041771a47958a0faf5b2f
Dhibiya <77314303+Dhibiya@users.noreply.github.com>
2021-09-28 12:28:05 +0530

    Program 9 Done By Dhibiya A

--- UserMainCode1.java
+++ UserMainCode1.java
@@ -1,30 +1,40 @@
-package Gaurav;
-import java.util.*;
-
-public class UserMainCode1{
-	static String getLargestWord(String s)
-	   {
-		   int t=0;
-		   String[] comp=s.split(" ");
-		   for(int i=0;i<comp.length;i++)
-		   {
-			   for(int j=i+1;i<comp.length;i++)
-			   if(comp[i].length()>comp[j].length())
-			 {
-				 t=i;
-			 }
-		   }
-		   return comp[t];
-	   }
-	public static void main(String[] args) 
+import java.util.Scanner;
+public class UserMainCode1 {
+	public static int addAndReverse(int brr[], int a)
 	{
-		String s;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a Sentence");
-		s=sc.nextLine();
-
-		String lar=UserMainCode1.getLargestWord(s);
-		System.out.println(lar);
+		int sum = 0, rem;
+		for(int i = 0 ; i < brr.length ; i++)
+		{
+			if(brr[i] > a )
+				sum += brr[i];
+		}
+		//System.out.println("Sum of the array : " +sum);
+		
+		int sum1 = 0;
+		while(sum!=0)
+		{
+			rem = sum %10;
+			sum1 = sum1*10 + rem;
+			sum /= 10;
+		}
+		return sum1;
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of elements to be enetred in an array : ");
+		int num = sc.nextInt();
+		int arr[] = new int[num];
+		System.out.println("Enter the element : ");
+		for(int i = 0 ; i < num ; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("Enter an element to compare : ");
+		int com = sc.nextInt();
+		
+		UserMainCode1 um = new UserMainCode1();
+		System.out.println(um.addAndReverse(arr,com));
 	}
 
 }
