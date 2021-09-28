9f2419d4222d5ee6d8e78b7a40ef9300ddb53bcb
Pragathi43 <91467365+Pragathi43@users.noreply.github.com>
2021-09-28 12:41:49 +0530

    program9 done by Pragathi

---
+++ UserMainCode_Pragathi.java
@@ -0,0 +1,43 @@
+package Assesment;
+import java.util.Scanner;
+public class UserMainCode_Pragathi {
+	public static int addAndReverse(int crr[],int x)
+	{
+		int s=0,r;
+		for(int i=0;i<crr.length;i++)
+		{
+			if(crr[i]>x)
+				s+=crr[i];
+		}
+		System.out.println(s);
+		int org=s,s1=0;
+		while(s!=0)
+		{
+			r=s%10;
+			s1=s1*10+r;
+			s/=10;
+			
+		}
+		return s1;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter number of elements that  in to array");
+		int num = sc.nextInt();
+		int brr[]=new int[num];
+		System.out.println("Enter element");
+	
+		for(int i=0;i<num;i++)
+		{
+			brr[i]=sc.nextInt();
+			
+		}
+		System.out.println("Enter element for compare");
+		int o=sc.nextInt();
+		
+		System.out.println(UserMainCode_Pragathi.addAndReverse(brr,o));
+
+	}
+
+}
