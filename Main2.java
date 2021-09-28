fa3a77c89201107c075c4108ec594fecbb5fc981
Abhishek Singh <65482891+abhishek250499@users.noreply.github.com>
2021-09-28 12:25:21 +0530

    Program 2 done by Abhishek Singh

---
+++ Main2.java
@@ -0,0 +1,16 @@
+package AbhishekSingh;
+//2
+import java.util.Scanner;
+
+public class Main2 {
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String st=sc.next();
+		int n=sc.nextInt();
+		UserMainCode2.formNewWord(st, n);
+
+	}
+
+}
--- UserMainCode2.java
+++ UserMainCode2.java
@@ -1,33 +1,19 @@
-import java.util.*;
-public class UserMainCode2 {
-
-	static String formNewWord(String s,int n)
+package AbhishekSingh;
+//2
+public class UserMainCode2
+{
+	public static void formNewWord(String str,int pnum) 
 	{
-		int len=s.length();
-		String new1=s.substring(0,n);
-		String new2=s.substring(len-n,len);
 
-		String new3=new1+new2;
-
-		String new4=new3.substring(0,1).toUpperCase();
-		String new5=new3.substring(1, new3.length());
-
-
-		return new4+new5;
-	}
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-
-		System.out.println("Enter a String");
-		String pass_str=sc.next();
-
-		System.out.println("Enter the splitting number");
-		int pass_num=sc.nextInt();
-
-		String get=UserMainCode2.formNewWord(pass_str, pass_num);
-
-		System.out.println(get);
+		int l=str.length();
+		if(l%2==0 && l>=2*pnum) {
+			String str1=str.substring(0, pnum);
+			String str2=str.substring(l-pnum, l);
+			System.out.println(str1+str2);
+		}
+		else {
+			System.out.println("Invalid Input");
+		}
 	}
 
 }
