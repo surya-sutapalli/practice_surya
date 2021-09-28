ec9123d9ef36ccd33ad54d3d0fd886b7988cf20d
bavatharani18 <67421617+bavatharani18@users.noreply.github.com>
2021-09-28 12:28:23 +0530

    Program 3 done by Bavatharani.C

--- UserMainCode1.java
+++ UserMainCode1.java
@@ -1,40 +1,39 @@
-import java.util.Scanner;
+import java.util.*;
 public class UserMainCode1 {
-	public static int addAndReverse(int brr[], int a)
-	{
-		int sum = 0, rem;
-		for(int i = 0 ; i < brr.length ; i++)
-		{
-			if(brr[i] > a )
-				sum += brr[i];
-		}
-		//System.out.println("Sum of the array : " +sum);
-		
-		int sum1 = 0;
-		while(sum!=0)
-		{
-			rem = sum %10;
-			sum1 = sum1*10 + rem;
-			sum /= 10;
-		}
-		return sum1;
-	}
+  public static int getLowest(HashMap<Integer,Integer> h)
+  { int sum=0,i=0;
+  int a[]= new int[h.size()];
+     Set<Integer> s= h.keySet();
+     Iterator<Integer> it= s.iterator();
+     while(it.hasNext())
+     {
+    	 int key=it.next();
+    	 a[i++]=(int)h.get(key);
+     }
+    Arrays.sort(a);
+    for(int j=0;j<3;j++)
+    {
+    	sum+=a[j];
+     }
+	return sum;  
+  }
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of elements to be enetred in an array : ");
-		int num = sc.nextInt();
-		int arr[] = new int[num];
-		System.out.println("Enter the element : ");
-		for(int i = 0 ; i < num ; i++)
-		{
-			arr[i] = sc.nextInt();
-		}
-		System.out.println("Enter an element to compare : ");
-		int com = sc.nextInt();
-		
-		UserMainCode1 um = new UserMainCode1();
-		System.out.println(um.addAndReverse(arr,com));
-	}
+     HashMap<Integer,Integer> h= new HashMap<Integer,Integer>();
+     Scanner sc= new Scanner(System.in);
+     System.out.println("Enter size of hashmap");
+     int n=sc.nextInt();
+     for(int i=0;i<n;i++)
+     {
+    	 System.out.println("Enter student rollnumber");
+    	 int num=sc.nextInt();
+    	 System.out.println("Enter marks");
+    	 int mark=sc.nextInt();
+    	 h.put(num, mark);
+     }
+     //System.out.println(h);
+    int result= UserMainCode1.getLowest(h);
+    System.out.println("Sum of lowest 3 marks: "+result);
+    sc.close();
+	}  
 
 }
